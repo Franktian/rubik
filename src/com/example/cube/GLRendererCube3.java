450a6c137b64187b68d4da29c6e1d8f01632887d
Yawen Tian <tianyawen201209@hotmail.com>
2014-03-19 23:46:45 -0400

    Change main page background color

--- src/com/example/cube/GLRendererCube3.java
+++ src/com/example/cube/GLRendererCube3.java
@@ -52,12 +52,12 @@ public class GLRendererCube3 implements GLSurfaceView.Renderer {
     private final float[] mProjectionMatrix = new float[16];
     private final float[] mViewMatrix = new float[16];
 //    private final float[] mRotationMatrix = new float[16];
-    private final float red[] = { 0.769f, 0.118f, 0.227f, 1.0f };
-    private final float green[] = { 0.0f, 0.62f, 0.376f, 1.0f };
-    private final float blue[] = { 0.0f, 0.318f, 0.729f, 1.0f };
-    private final float white[] = { 1.0f, 1.0f, 1.0f, 1.0f };
-    private final float yellow[] = { 1.0f, 0.835f, 0.0f, 1.0f };
-    private final float orange[] = { 1.0f, 0.345f, 0.0f, 1.0f };
+    private final float red[] = { 0.769f, 0.118f, 0.227f, 0.8f };
+    private final float green[] = { 0.0f, 0.62f, 0.376f, 0.8f };
+    private final float blue[] = { 0.0f, 0.318f, 0.729f, 0.8f };
+    private final float white[] = { 0.898f, 0.898f, 0.898f, 0.8f };
+    private final float yellow[] = { 1.0f, 0.835f, 0.0f, 0.8f };
+    private final float orange[] = { 1.0f, 0.345f, 0.0f, 0.8f };
     public static Cube3 myCube3 = new Cube3();
 
     private float top_one_color[] = null;
@@ -91,7 +91,7 @@ public class GLRendererCube3 implements GLSurfaceView.Renderer {
     @Override
     public void onSurfaceCreated(GL10 unused, EGLConfig config) {
         // Set the background frame color
-        GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        GLES20.glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
         
         mSquare3_Top_One = new Square3_Top_One();
         mSquare3_Top_Two = new Square3_Top_Two();
