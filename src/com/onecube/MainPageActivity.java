b7a78b60ed66a9419dce9762080dc0c55e0420b1
jason0997 <417703557@qq.com>
2014-04-26 18:40:56 -0400

    Modified Back Button and Add a Setting Button in Cube2

--- src/com/onecube/MainPageActivity.java
+++ src/com/onecube/MainPageActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.onecube.util.SystemUiHider;
 
@@ -21,12 +22,20 @@ public class MainPageActivity extends Activity {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.activity_main_page);
+		final Button backbutton  = (Button) findViewById(R.id.back_button);
+		backbutton.setBackgroundResource(R.drawable.backtomenu);
+		final Button settingbutton  = (Button) findViewById(R.id.setting_button);
+		settingbutton.setBackgroundResource(R.drawable.settingplaying);
 	}
 
     public void backView(View view) {
 		Intent intent = new Intent(this, SubmenuActivity.class);
 	    startActivity(intent);
     }
+	public void startSetting(View view){
+		Intent intent = new Intent(this, SettingActivity.class);
+	    startActivity(intent);		
+	}
 	
     @Override
     public void onBackPressed() {
